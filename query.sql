fc8055875b82378074b5d5f9e2788355dbf22fc1
Kaloo Ina <kaloina.rakotobe@gmail.com>
2025-12-14 22:24:46 +0100

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,23 +1,27 @@
 use WideWorldImporters
 
-/* Orders placed on the 26th june 2015 */
+/* 1 . Orders placed on the 26th june 2015 */
 select count( SO.OrderID )
 from Sales.Orders as SO
 where SO.OrderDate = '2015-06-26'
 
-/* Total transaction amount done by the client whose CustomerId = 1 */
+/* 2 . Total transaction amount done by the client whose CustomerId = 1 */
 select sum( SCT.TransactionAmount )
 from Sales.CustomerTransactions as SCT
 where SCT.CustomerID = 1
 
-/* Average tax amount */
+/* 3 . Average tax amount */
 select avg( SCT.TaxAmount )
 from Sales.CustomerTransactions as SCT
 
-/* Display all the details of the StockItemID "10" , last edited from 1st january 2013 to 31st december 2013 */
+/* 4 . Display all the details of the StockItemID "10" , last edited from 1st january 2013 to 31st december 2013 */
 select *
 from Sales.InvoiceLines as SIL
 where ( SIL.StockItemID = 10 ) and (( SIL.LastEditedWhen ) between '2013-01-01' and '2013-12-31' )
 
+/* 5 . Display the average unit price of all the StockItemIDs where the average line profit exceeds 150 */
 
-
+select SIL.StockItemID , avg( SIL.UnitPRice )
+from Sales.InvoiceLines as SIL
+group by SIL.StockItemID
+having avg( SIL.LineProfit ) > 150
